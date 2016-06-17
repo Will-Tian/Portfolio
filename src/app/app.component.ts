7bc17211bf49825ded271fed5cc38a74e03dfe2c
Will-Tian <will.w.tian@gmail.com>
2016-06-16 18:10:48 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import '../../public/css/styles.css';
+import {HomeComponent} from './home/home.component.ts';
+@Component({
+  selector: 'portfolio-app',
+  template: require('./app.component.html'),
+  styles: [require('./app.component.css')],
+  directives: [ROUTER_DIRECTIVES, HomeComponent],
+  providers: [ROUTER_PROVIDERS]
+})
+@RouteConfig([
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomeComponent,
+    useAsDefault: true
+  },
+])
+export class AppComponent { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  template: require('./home.component.html'),
+  styles: [require('./home.component.css')],
+  directives: []
+})
+
+export class HomeComponent{
+}
