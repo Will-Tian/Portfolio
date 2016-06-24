7913e118fbdd5fb2412d26eae3ac93a77884927d
Will-Tian <will.w.tian@gmail.com>
2016-06-24 15:29:00 -0700

    adds image assets;create progress bar animations; create animation wrappers

--- src/app/home/home1.component.ts
+++ src/app/home/home1.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 import {ScrollService} from '../directive-services/scroll.service';
 
 @Component({
@@ -8,4 +8,71 @@ import {ScrollService} from '../directive-services/scroll.service';
 })
 
 export class Home1Component{
+	currentSlide = 0;
+	slideNum = 2;
+	slideColors = ['rgba(85, 178, 220, 0.95)', 'rgba(48, 33, 19, 0.95)', 'rgba(101, 171, 123, 0.95)'];
+	private _slideAnimationInterval: any;
+	private _progressBarInterval: any;
+
+	ngOnInit() { this.initialize(); }
+	ngOnDestroy() { this.cleanUp(); }
+
+	initialize() {
+		this._startSlideAnimationInterval();
+		this._startProgressBarInterval();
+	}
+
+	cleanUp() {
+		if(this._slideAnimationInterval){ clearInterval(this._slideAnimationInterval);}
+		if(this._progressBarInterval){clearInterval(this._progressBarInterval);}
+	}	
+
+	private _startSlideAnimationInterval(){
+		this._slideAnimationInterval = window.setInterval(this._slideAnimation.bind(this), 4900);
+	}
+
+	private _slideAnimation(){
+		this._slideTransition();
+		this._slideColorTransition();
+	}
+
+	private _slideTransition(){
+		if(this.currentSlide >= this.slideNum){this.currentSlide = 0;return;}
+		this.currentSlide += 1;
+	}
+
+	private _slideColorTransition(){
+		var index = this.currentSlide;
+		var color = this.slideColors[index];
+		$('#js-home--mask').css('fill', color);
+	}
+	private _timer(t){
+		return new Promise(function(resolve, reject) {
+		    setTimeout(function() {
+		        resolve();
+		    }, t);
+		});
+	}
+	private _startProgressBarInterval(){
+		this._progressBarAnimation();
+		this._progressBarInterval = window.setInterval(this._progressBarAnimation.bind(this), 4900);
+	}
+	private _progressBarAnimation(){
+		window.setTimeout(this._progressBarExpand, 0);
+		window.setTimeout(this._progressBarFull, 4200);
+		window.setTimeout(this._progressBarClosing, 4300);
+		window.setTimeout(this._progressBarReset, 4800);
+	}
+	private _progressBarExpand(){
+		$('#progress-bar').addClass('expanding');
+	}
+	private _progressBarFull(){
+		$('#progress-bar').addClass('full');	
+	}
+	private _progressBarClosing(){
+		$('#progress-bar').addClass('closing');		
+	}
+	private _progressBarReset(){
+		$('#progress-bar').removeClass('expanding full closing');		
+	}
 }
